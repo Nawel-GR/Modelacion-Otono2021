255fac9b96b39b12b8956f83b13aad2bbc352f34
Nawel-GR <nahuelgomez.r@gmail.com>
2021-03-30 21:25:52 -0300

    Comentarios
    Arreglo de los comentarios del códico

--- Ejercicio 2/ejercicio2.py
+++ Ejercicio 2/ejercicio2.py
@@ -8,9 +8,9 @@ class Controller:
     """
     Clase controlador que guardar las variables a modificar en la funcion on_key
     """
-    fillPolygon = True # Variable que indica si se debe dibujar en modo lineas o rellenar los triangulos
-    effect1 = False # Variable que guarda el estado del efecto 1 : verde (activado o desactivado)
-    effect2 = False # Variable que guarda el estado del efecto 1 : atardecer (activado o desactivado)
+    fillPolygon = True 
+    effect1 = False 
+    effect2 = False 
 
 
 # we will use the global controller as communication with the callback function
@@ -33,11 +33,11 @@ def on_key(window, key, scancode, action, mods):
     elif key == glfw.KEY_ESCAPE:
         glfw.set_window_should_close(window, True)
 
-    # Si detecta la tecla [Q] cambia el estado del efecto 1 : verde
+    # Si detecta la tecla [Q] cambia el estado del efecto 1 : Achicar
     elif key == glfw.KEY_Q:
         controller.effect1 = not controller.effect1
 
-    # Si detecta la tecla [W] cambia el estado del efecto 2 : atardecer
+    # Si detecta la tecla [W] cambia el estado del efecto 2 : Anochecer
     elif key == glfw.KEY_W:
         controller.effect2 = not controller.effect2
 
@@ -125,15 +125,15 @@ class SimpleShaderProgram:
         # Unbind the current VAO
         glBindVertexArray(0)
 
-class GreenShaderProgram:
+class NocheShaderProgram:
     """
     Clase para guardar el los shaders compilados
-    Contiene los shaders para el efecto
+    Contiene los shaders para el efecto de achicar
     """
 
     def __init__(self):
 
-        # Vertex shader no de modifica
+        # Vertex shader no se modifica
         vertex_shader = """
             #version 130
 
@@ -147,7 +147,7 @@ class GreenShaderProgram:
                 newColor = color;
             }
             """
-        # Se modifica el fragment shader para descartar los fragmentos que tienen un componente verde bajo
+        # Se modifica el fragment shader para cambiar los colores a una tonalidad oscura
         fragment_shader = """
             #version 130
             in vec3 newColor;
@@ -156,7 +156,7 @@ class GreenShaderProgram:
             void main()
             {   
                 float newRed = newColor.r - 0.6; // Se modifica la componente roja
-                float newGreen = newColor.g - 0.6; // Se modifica la componente verde
+                float newGreen = newColor.g - 0.6; // Se modifica la componente Achicar
                 float newBlue = newColor.b - 0.5; // Se modifica la componente azul
                 vec3 finalColor = vec3(newRed, newGreen, newBlue); // Se crea el nuevo vector rgb
                 outColor = vec4(finalColor, 1.0f); // Se asigna el color final
@@ -178,7 +178,7 @@ class GreenShaderProgram:
         glBindBuffer(GL_ARRAY_BUFFER, gpuShape.vbo)
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gpuShape.ebo)
 
-        # 3d vertices + rgb color specification => 3*4 + 3*4 = 24 bytes
+        
         position = glGetAttribLocation(self.shaderProgram, "position")
         glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
         glEnableVertexAttribArray(position)
@@ -204,13 +204,14 @@ class GreenShaderProgram:
         # Unbind the current VAO
         glBindVertexArray(0)
 
-class SunsetShaderProgram:
+class ChicoShaderProgram:
     """
     Clase para guardar el los shaders compilados
     Contiene los shaders basicos (sin efectos)
     """
     def __init__(self):
-
+        
+        #Se modifican el vertex shader para achicar la imagen
         vertex_shader = """
             #version 130
 
@@ -252,7 +253,7 @@ class SunsetShaderProgram:
         glBindBuffer(GL_ARRAY_BUFFER, gpuShape.vbo)
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gpuShape.ebo)
 
-        # 3d vertices + rgb color specification => 3*4 + 3*4 = 24 bytes
+        
         position = glGetAttribLocation(self.shaderProgram, "position")
         glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
         glEnableVertexAttribArray(position)
@@ -282,9 +283,6 @@ def create_sky(y0, y1):
     """
     Funcion para crear rectangulo que represente el cielo
 
-    Parameters:
-    y_0 (float): altura inferior donde empezara el rectangulo
-    y_1 (float): altura superior donde termina el rectangulo
     """
     # Defining the location and colors of each vertex  of the shape
     vertices = [
@@ -302,13 +300,11 @@ def create_sky(y0, y1):
     return Shape(vertices, indices)
 
 
-def create_ocean(y0, y1):
+def create_pilar(y0, y1):
     """
-    Funcion para crear rectangulo que represente el oceano
+    Funcion para crear rectangulo que represente el pilar de la corona
 
-    Parameters:
-    y_0 (float): altura inferior donde empezara el rectangulo
-    y_1 (float): altura superior donde termina el rectangulo
+
     """
     # Defining the location and colors of each vertex  of the shape
     vertices = [
@@ -326,9 +322,9 @@ def create_ocean(y0, y1):
     return Shape(vertices, indices)
     
 
-def create_island(y0, y1):
+def create_corsup(y0, y1):
     """
-    Funcion para crear rectangulo que represente el oceano
+    Funcion para crear rectangulo que represente la parte superior de la corona
 
     """
     # Defining the location and colors of each vertex  of the shape
@@ -347,9 +343,9 @@ def create_island(y0, y1):
     return Shape(vertices, indices)
 
 
-def create_volcano(y0, y1):
+def create_corinf(y0, y1):
     """
-    Funcion para crear rectangulo que represente el oceano
+    Funcion para crear rectangulo que represente la parte inferior de la corona
 
     """
     # Defining the location and colors of each vertex  of the shape
@@ -369,7 +365,7 @@ def create_volcano(y0, y1):
 
 def create_triangulo(y0,y1):
     """
-    Funcion para crear rectangulo que represente el oceano
+    Funcion para crear rectangulo que represente los triangulos de la corona
 
     """
     # Defining the location and colors of each vertex  of the shape
@@ -462,8 +458,8 @@ if __name__ == "__main__":
     
     # Creamos las instancias de los ShaderProgram, que compila los shaders creados
     simplePipeline = SimpleShaderProgram() # Shaders normales
-    greenPipeline = GreenShaderProgram() # Shaders para efecto verde
-    sunsetPipeline = SunsetShaderProgram() #Shaders para efecto atardecer
+    ChicoPipeline = ChicoShaderProgram() # Shaders para efecto achicar
+    NochePipeline = NocheShaderProgram() #Shaders para efecto anochecer
 
     """
     Se crea cada figura en la memoria de la GPU.
@@ -475,56 +471,56 @@ if __name__ == "__main__":
     sky_shape = create_sky(y0=-1, y1=1.0) # Creamos los vertices e indices (guardandolos en un objeto shape)
     gpu_sky = GPUShape().initBuffers() # Se le pide memoria a la GPU para guardar la figura
     simplePipeline.setupVAO(gpu_sky) # Se le dice al ShaderProgram NORMAL como leer esta parte de la memoria 
-    greenPipeline.setupVAO(gpu_sky) # Se le dice al ShaderProgram del EFECTO 1 (VERDE) como leer esta parte de la memoria 
-    sunsetPipeline.setupVAO(gpu_sky) # Se le dice al ShaderProgram del EFECTO 2 (ATARDECER) como leer esta parte de la memoria 
+    ChicoPipeline.setupVAO(gpu_sky) # Se le dice al ShaderProgram del EFECTO 1 (Achicar) como leer esta parte de la memoria 
+    NochePipeline.setupVAO(gpu_sky) # Se le dice al ShaderProgram del EFECTO 2 (Anochecer) como leer esta parte de la memoria 
     gpu_sky.fillBuffers(sky_shape.vertices, sky_shape.indices, GL_STATIC_DRAW) # Llenamos esta memoria de la GPU con los vertices e indices
 
-    # 2- Creamos la Figura del oceano en la GPU
-    ocean_shape = create_ocean(y0=-1, y1=0.0) # Creamos los vertices e indices (guardandolos en un objeto shape)
-    gpu_ocean = GPUShape().initBuffers() # Se le pide memoria a la GPU para guardar la figura
-    simplePipeline.setupVAO(gpu_ocean) # Se le dice al ShaderProgram NORMAL como leer esta parte de la memoria 
-    greenPipeline.setupVAO(gpu_ocean) # Se le dice al ShaderProgram del EFECTO 1 (VERDE) como leer esta parte de la memoria 
-    sunsetPipeline.setupVAO(gpu_ocean) # Se le dice al ShaderProgram del EFECTO 2 (ATARDECER) como leer esta parte de la memoria 
-    gpu_ocean.fillBuffers(ocean_shape.vertices, ocean_shape.indices, GL_STATIC_DRAW) # Llenamos esta memoria de la GPU con los vertices e indices
+    # 2- Creamos la Figura del pilar en la GPU
+    pilar_shape = create_pilar(y0=-1, y1=0.0) # Creamos los vertices e indices (guardandolos en un objeto shape)
+    gpu_pilar = GPUShape().initBuffers() # Se le pide memoria a la GPU para guardar la figura
+    simplePipeline.setupVAO(gpu_pilar) # Se le dice al ShaderProgram NORMAL como leer esta parte de la memoria 
+    ChicoPipeline.setupVAO(gpu_pilar) # Se le dice al ShaderProgram del EFECTO 1 (Achicar) como leer esta parte de la memoria 
+    NochePipeline.setupVAO(gpu_pilar) # Se le dice al ShaderProgram del EFECTO 2 (Anochecer) como leer esta parte de la memoria 
+    gpu_pilar.fillBuffers(pilar_shape.vertices, pilar_shape.indices, GL_STATIC_DRAW) # Llenamos esta memoria de la GPU con los vertices e indices
 
-     # 3- Creamos la Figura de la isla en la GPU
-    island_shape = create_island(y0=0.075, y1=0.4) # Creamos los vertices e indices (guardandolos en un objeto shape)
-    gpu_island = GPUShape().initBuffers() # Se le pide memoria a la GPU para guardar la figura
-    simplePipeline.setupVAO(gpu_island) # Se le dice al ShaderProgram NORMAL como leer esta parte de la memoria 
-    greenPipeline.setupVAO(gpu_island) # Se le dice al ShaderProgram del EFECTO 1 (VERDE) como leer esta parte de la memoria 
-    sunsetPipeline.setupVAO(gpu_island) # Se le dice al ShaderProgram del EFECTO 2 (ATARDECER) como leer esta parte de la memoria 
-    gpu_island.fillBuffers(island_shape.vertices, island_shape.indices, GL_STATIC_DRAW) # Llenamos esta memoria de la GPU con los vertices e indices
+     # 3- Creamos la Figura de la corinf en la GPU
+    corsup_shape = create_corsup(y0=0.075, y1=0.4) # Creamos los vertices e indices (guardandolos en un objeto shape)
+    gpu_corsup = GPUShape().initBuffers() # Se le pide memoria a la GPU para guardar la figura
+    simplePipeline.setupVAO(gpu_corsup) # Se le dice al ShaderProgram NORMAL como leer esta parte de la memoria 
+    ChicoPipeline.setupVAO(gpu_corsup) # Se le dice al ShaderProgram del EFECTO 1 (Achicar) como leer esta parte de la memoria 
+    NochePipeline.setupVAO(gpu_corsup) # Se le dice al ShaderProgram del EFECTO 2 (Anochecer) como leer esta parte de la memoria 
+    gpu_corsup.fillBuffers(corsup_shape.vertices, corsup_shape.indices, GL_STATIC_DRAW) # Llenamos esta memoria de la GPU con los vertices e indices
 
-    # 4- Creamos la Figura del volcan en la GPU 
-    volcano_shape = create_volcano(y0=0.0, y1=0.075) # Creamos los vertices e indices (guardandolos en un objeto shape)
-    gpu_volcano = GPUShape().initBuffers() # Se le pide memoria a la GPU para guardar la figura
-    simplePipeline.setupVAO(gpu_volcano) # Se le dice al ShaderProgram NORMAL como leer esta parte de la memoria 
-    greenPipeline.setupVAO(gpu_volcano) # Se le dice al ShaderProgram del EFECTO 1 (VERDE) como leer esta parte de la memoria 
-    sunsetPipeline.setupVAO(gpu_volcano) # Se le dice al ShaderProgram del EFECTO 2 (ATARDECER) como leer esta parte de la memoria 
-    gpu_volcano.fillBuffers(volcano_shape.vertices, volcano_shape.indices, GL_STATIC_DRAW) # Llenamos esta memoria de la GPU con los vertices e indices
+    # 4- Creamos la Figura del corinf en la GPU 
+    corinf_shape = create_corinf(y0=0.0, y1=0.075) # Creamos los vertices e indices (guardandolos en un objeto shape)
+    gpu_corinf = GPUShape().initBuffers() # Se le pide memoria a la GPU para guardar la figura
+    simplePipeline.setupVAO(gpu_corinf) # Se le dice al ShaderProgram NORMAL como leer esta parte de la memoria 
+    ChicoPipeline.setupVAO(gpu_corinf) # Se le dice al ShaderProgram del EFECTO 1 (Achicar) como leer esta parte de la memoria 
+    NochePipeline.setupVAO(gpu_corinf) # Se le dice al ShaderProgram del EFECTO 2 (Anochecer) como leer esta parte de la memoria 
+    gpu_corinf.fillBuffers(corinf_shape.vertices, corinf_shape.indices, GL_STATIC_DRAW) # Llenamos esta memoria de la GPU con los vertices e indices
 
      # 5- Creamos la Figura del triangulo en la GPU
     triangulo_shape = create_triangulo(y0=0.3, y1=0.4) # Creamos los vertices e indices (guardandolos en un objeto shape)
     gpu_triangulo = GPUShape().initBuffers() # Se le pide memoria a la GPU para guardar la figura
     simplePipeline.setupVAO(gpu_triangulo) # Se le dice al ShaderProgram NORMAL como leer esta parte de la memoria 
-    greenPipeline.setupVAO(gpu_triangulo) # Se le dice al ShaderProgram del EFECTO 1 (VERDE) como leer esta parte de la memoria 
-    sunsetPipeline.setupVAO(gpu_triangulo) # Se le dice al ShaderProgram del EFECTO 2 (ATARDECER) como leer esta parte de la memoria 
+    ChicoPipeline.setupVAO(gpu_triangulo) # Se le dice al ShaderProgram del EFECTO 1 (Achicar) como leer esta parte de la memoria 
+    NochePipeline.setupVAO(gpu_triangulo) # Se le dice al ShaderProgram del EFECTO 2 (Anochecer) como leer esta parte de la memoria 
     gpu_triangulo.fillBuffers(triangulo_shape.vertices, triangulo_shape.indices, GL_STATIC_DRAW) # Llenamos esta memoria de la GPU con los vertices e indices
 
     # 6- Creamos la Figura de la franja en la GPU 
     franja_shape = create_franja(y0=0.06, y1=0.07) # Creamos los vertices e indices (guardandolos en un objeto shape)
     gpu_franja = GPUShape().initBuffers() # Se le pide memoria a la GPU para guardar la figura
     simplePipeline.setupVAO(gpu_franja) # Se le dice al ShaderProgram NORMAL como leer esta parte de la memoria 
-    greenPipeline.setupVAO(gpu_franja) # Se le dice al ShaderProgram del EFECTO 1 (VERDE) como leer esta parte de la memoria 
-    sunsetPipeline.setupVAO(gpu_franja) # Se le dice al ShaderProgram del EFECTO 2 (ATARDECER) como leer esta parte de la memoria 
+    ChicoPipeline.setupVAO(gpu_franja) # Se le dice al ShaderProgram del EFECTO 1 (Achicar) como leer esta parte de la memoria 
+    NochePipeline.setupVAO(gpu_franja) # Se le dice al ShaderProgram del EFECTO 2 (Anochecer) como leer esta parte de la memoria 
     gpu_franja.fillBuffers(franja_shape.vertices, franja_shape.indices, GL_STATIC_DRAW)
 
     # 6- Creamos la Figura de rubi en la GPU 
     rubi_shape = create_rubi(y0=0.1, y1=0.175, y2=0.25) # Creamos los vertices e indices (guardandolos en un objeto shape)
     gpu_rubi = GPUShape().initBuffers() # Se le pide memoria a la GPU para guardar la figura
     simplePipeline.setupVAO(gpu_rubi) # Se le dice al ShaderProgram NORMAL como leer esta parte de la memoria 
-    greenPipeline.setupVAO(gpu_rubi) # Se le dice al ShaderProgram del EFECTO 1 (VERDE) como leer esta parte de la memoria 
-    sunsetPipeline.setupVAO(gpu_rubi) # Se le dice al ShaderProgram del EFECTO 2 (ATARDECER) como leer esta parte de la memoria 
+    ChicoPipeline.setupVAO(gpu_rubi) # Se le dice al ShaderProgram del EFECTO 1 (Achicar) como leer esta parte de la memoria 
+    NochePipeline.setupVAO(gpu_rubi) # Se le dice al ShaderProgram del EFECTO 2 (Anochecer) como leer esta parte de la memoria 
     gpu_rubi.fillBuffers(rubi_shape.vertices, rubi_shape.indices, GL_STATIC_DRAW)
 
 
@@ -547,39 +543,39 @@ if __name__ == "__main__":
 
         """
         Si esta el efecto 1 activado (se activa o desactiva con la tecla [T]
-        Se usa el shaderProgram del efecto 1: verde
+        Se usa el shaderProgram del efecto 1: Achicar
         """
         if (controller.effect1):
-            # Se le dice a OpenGL que use el shaderProgram del efecto 1: verde
-            glUseProgram(greenPipeline.shaderProgram)
-            greenPipeline.drawCall(gpu_sky) # Se dibuja el cielo
-            greenPipeline.drawCall(gpu_ocean) # Se dibuja el oceano
-            greenPipeline.drawCall(gpu_island) # Se dibuja la isla
-            greenPipeline.drawCall(gpu_volcano) # Se dibuja el volcan
-            greenPipeline.drawCall(gpu_triangulo) # Se dibuja el triangulo
-            greenPipeline.drawCall(gpu_franja) # Se dibuja de la franja
-            greenPipeline.drawCall(gpu_rubi) # Se dibuja el rubi
+            # Se le dice a OpenGL que use el shaderProgram del efecto 1: Achicar
+            glUseProgram(ChicoPipeline.shaderProgram)
+            ChicoPipeline.drawCall(gpu_sky) # Se dibuja el cielo
+            ChicoPipeline.drawCall(gpu_pilar) # Se dibuja el pilar
+            ChicoPipeline.drawCall(gpu_corsup) # Se dibuja la corinf
+            ChicoPipeline.drawCall(gpu_corinf) # Se dibuja el corinf
+            ChicoPipeline.drawCall(gpu_triangulo) # Se dibuja el triangulo
+            ChicoPipeline.drawCall(gpu_franja) # Se dibuja de la franja
+            ChicoPipeline.drawCall(gpu_rubi) # Se dibuja el rubi
         # Si esta el efecto 2 activado (se activa o desactiva con la tecla [W]
-        # Se usa el shaderProgram del efecto 2: atardecer
+        # Se usa el shaderProgram del efecto 2: Anochecer
         elif (controller.effect2):
-            # Se le dice a OpenGL que use el shaderProgram del efecto 2: atardecer
-            glUseProgram(sunsetPipeline.shaderProgram)
-            sunsetPipeline.drawCall(gpu_sky) # Se dibuja el cielo
-            sunsetPipeline.drawCall(gpu_ocean) # Se dibuja el oceano
-            sunsetPipeline.drawCall(gpu_island) # Se dibuja la isla
-            sunsetPipeline.drawCall(gpu_volcano) # Se dibuja el volcan
-            sunsetPipeline.drawCall(gpu_triangulo) # Se dibuja el triangulo
-            sunsetPipeline.drawCall(gpu_franja) # Se dibuja la franja
-            sunsetPipeline.drawCall(gpu_rubi) # Se dibuja rubi
+            # Se le dice a OpenGL que use el shaderProgram del efecto 2: Anochecer
+            glUseProgram(NochePipeline.shaderProgram)
+            NochePipeline.drawCall(gpu_sky) # Se dibuja el cielo
+            NochePipeline.drawCall(gpu_pilar) # Se dibuja el pilar
+            NochePipeline.drawCall(gpu_corsup) # Se dibuja la corinf
+            NochePipeline.drawCall(gpu_corinf) # Se dibuja el corinf
+            NochePipeline.drawCall(gpu_triangulo) # Se dibuja el triangulo
+            NochePipeline.drawCall(gpu_franja) # Se dibuja la franja
+            NochePipeline.drawCall(gpu_rubi) # Se dibuja rubi
         # Si no hay un efecto activado
         # Se usa el shaderProgram normal
         else:
             # Se le dice a OpenGL que use el shaderProgram normal
             glUseProgram(simplePipeline.shaderProgram)
             simplePipeline.drawCall(gpu_sky) # Se dibuja el cielo
-            simplePipeline.drawCall(gpu_ocean) # Se dibuja el oceano
-            simplePipeline.drawCall(gpu_island) # Se dibuja la isla
-            simplePipeline.drawCall(gpu_volcano) # Se dibuja el volcan
+            simplePipeline.drawCall(gpu_pilar) # Se dibuja el pilar
+            simplePipeline.drawCall(gpu_corsup) # Se dibuja la corinf
+            simplePipeline.drawCall(gpu_corinf) # Se dibuja el corinf
             simplePipeline.drawCall(gpu_triangulo) # Se dibuja el triangulo
             simplePipeline.drawCall(gpu_franja) # Se dibuja ela franja
             simplePipeline.drawCall(gpu_rubi) # Se dibuja ela rubi
@@ -589,9 +585,9 @@ if __name__ == "__main__":
 
     # freeing GPU memory
     gpu_sky.clear()
-    gpu_ocean.clear()
-    gpu_island.clear()
-    gpu_volcano.clear()
+    gpu_pilar.clear()
+    gpu_corsup.clear()
+    gpu_corinf.clear()
     gpu_triangulo.clear()
     gpu_franja.clear()
     gpu_rubi.clear()
