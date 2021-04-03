3dab5da94264dea4bf440ec77cf8bde5354f0637
Nawel-GR <nahuelgomez.r@gmail.com>
2021-04-03 14:43:00 -0300

    Traslacion de la tierra
    Se sube la traslación de la tierra entorno al sol

--- Ejercicio 3/grafica-main/examples/Ejercicio_3.py
+++ Ejercicio 3/grafica-main/examples/Ejercicio_3.py
@@ -10,6 +10,7 @@ import os.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import grafica.gpu_shape as gs
 import grafica.transformations as tr
+import math
 from grafica.gpu_shape import GPUShape
 
 
@@ -70,7 +71,7 @@ def createCircle(N):
     gpuShape = GPUShape()
 
     # First vertex at the center, white color
-    vertices = [0, 0, 0, 1.0, 1.0, 1.0]
+    vertices = [0, 0, 0, 1.0, 1.0, 0.0]
     indices = []
 
     dtheta = 2 * np.pi / N
@@ -83,7 +84,7 @@ def createCircle(N):
             0.5 * np.cos(theta), 0.5 * np.sin(theta), 0,
 
             # color generates varying between 0 and 1
-                  np.sin(theta),       np.cos(theta), 0]
+                  0,       0, 0]
 
         # A triangle is created using the center, this and the next vertex
         indices += [0, i, i+1]
@@ -109,44 +110,7 @@ def createCircle(N):
 
     return gpuShape
 
-def createQuad():
 
-    # Here the new shape will be stored
-    gpuShape = gs.GPUShape()
-
-    # Defining locations and colors for each vertex of the shape
-    
-    vertexData = np.array([
-    #   positions        colors
-        -0.5, -0.5, 0.0,  1.0, 0.0, 0.0,
-         0.5, -0.5, 0.0,  0.0, 1.0, 0.0,
-         0.5,  0.5, 0.0,  0.0, 0.0, 1.0,
-        -0.5,  0.5, 0.0,  1.0, 1.0, 1.0
-    # It is important to use 32 bits data
-        ], dtype = np.float32)
-
-    # Defining connections among vertices
-    # We have a triangle every 3 indices specified
-    indices = np.array(
-        [0, 1, 2,
-         2, 3, 0], dtype= np.uint32)
-
-    gpuShape.size = len(indices)
-
-    # VAO, VBO and EBO and  for the shape
-    gpuShape.vao = glGenVertexArrays(1)
-    gpuShape.vbo = glGenBuffers(1)
-    gpuShape.ebo = glGenBuffers(1)
-
-    # Vertex data must be attached to a Vertex Buffer Object (VBO)
-    glBindBuffer(GL_ARRAY_BUFFER, gpuShape.vbo)
-    glBufferData(GL_ARRAY_BUFFER, len(vertexData) * SIZE_IN_BYTES, vertexData, GL_STATIC_DRAW)
-
-    # Connections among vertices are stored in the Elements Buffer Object (EBO)
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gpuShape.ebo)
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, len(indices) * SIZE_IN_BYTES, indices, GL_STATIC_DRAW)
-
-    return gpuShape
 
 
 if __name__ == "__main__":
@@ -210,8 +174,7 @@ if __name__ == "__main__":
     glClearColor(0.15, 0.15, 0.15, 1.0)
 
     # Creating shapes on GPU memory
-    gpuCircle = createCircle(30)
-    gpuQuad = createQuad()
+    gpuCircle = createCircle(10)
 
     while not glfw.window_should_close(window):
         # Using GLFW to check for input events
@@ -229,11 +192,10 @@ if __name__ == "__main__":
         # Using the time as the theta parameter
         theta = glfw.get_time()
 
-        # Circle
+        # Sol // centro
         circleTransform = tr.matmul([
-            tr.translate(0.5, 0.5, 0),
-            tr.rotationZ(2 * theta),
-            tr.uniformScale(0.5)
+            tr.translate(0.0, 0.0, 0.0),
+            tr.uniformScale(0.70)
         ])
 
         # updating the transform attribute
@@ -242,40 +204,29 @@ if __name__ == "__main__":
         # drawing function
         drawCall(shaderProgram, gpuCircle)
 
-        # Second instance of the circle
+        # Tierra //arriba a la izq
         circleTransform2 = tr.matmul([
-            tr.translate(-0.5, 0.5, 0),
-            tr.scale(
-                0.5 + 0.2 * np.cos(1.5 * theta),
-                0.5 + 0.2 * np.sin(2 * theta),
-                0)
+            tr.translate(math.cos(theta)*3/4, math.sin(theta)*3/4, 0),
+            tr.rotationZ(-theta),
+            tr.uniformScale(0.20)
         ])
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "transform"), 1, GL_TRUE, circleTransform2)
         drawCall(shaderProgram, gpuCircle)
 
         #Third instance of the circle
         circleTransform3 = tr.matmul([
-            tr.translate(-0.5, -0.5, 0),
+            tr.translate(math.cos(theta), math.sin(theta), 0),
             tr.rotationZ(-theta),
-            tr.uniformScale(0.7)
+            tr.uniformScale(0.1)
         ])
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "transform"), 1, GL_TRUE, circleTransform3)
         drawCall(shaderProgram, gpuCircle)
 
-        #Fourth
-        circleTransform4 = tr.matmul([
-            tr.translate(0.5, -0.5, 0),
-            tr.shearing(0.3 * np.cos(theta), 0, 0, 0, 0, 0),
-            tr.uniformScale(0.7)
-        ])
-        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "transform"), 1, GL_TRUE, circleTransform4)
-        drawCall(shaderProgram, gpuCircle)
 
-        # Once the drawing is rendered, buffers are swap so an uncomplete drawing is never seen.
         glfw.swap_buffers(window)
 
     # freeing GPU memory
     gpuCircle.clear()
-    gpuQuad.clear()
+
     
     glfw.terminate()
