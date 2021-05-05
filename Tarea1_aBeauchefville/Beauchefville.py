37b053c29a0f9fa2b925e405b635a14c8a8185b5
Nawel-GR <nahuelgomez.r@gmail.com>
2021-05-05 19:41:22 -0400

    Esqueleto
    Esqueleto de la tarea

---
+++ Tarea1_aBeauchefville/Beauchefville.py
@@ -0,0 +1,64 @@
+"""Tarea 1A BeauchefVille"""
+
+
+import glfw
+import OpenGL.GL.shaders
+import numpy as np
+import grafica.basic_shapes as bs
+import grafica.easy_shaders as es
+import grafica.transformations as tr
+import grafica.performance_monitor as pm
+import grafica.scene_graph as sg
+from shapes import *
+from model import *
+
+
+#Se agrega el controllador de nuestro personaje
+class Controller:
+    def __init__(self):
+        self.fillPolygon = True
+        self.is_w_pressed = False
+        self.is_s_pressed = False
+        self.is_a_pressed = False
+        self.is_d_pressed = False
+
+controller = Controller()
+
+def on_key(window, key, scancode, action, mods):
+    
+    global controller
+    
+    # Tecla "W"
+    if key == glfw.KEY_W:
+        if action ==glfw.PRESS:
+            controller.is_w_pressed = True
+        elif action == glfw.RELEASE:
+            controller.is_w_pressed = False
+
+    # Tecla "S"
+    if key == glfw.KEY_S:
+        if action ==glfw.PRESS:
+            controller.is_s_pressed = True
+        elif action == glfw.RELEASE:
+            controller.is_s_pressed = False
+
+    # Tecla "A"
+    if key == glfw.KEY_A:
+        if action ==glfw.PRESS:
+            controller.is_a_pressed = True
+        elif action == glfw.RELEASE:
+            controller.is_a_pressed = False
+
+    # Tecla "D"
+    if key == glfw.KEY_D:
+        if action ==glfw.PRESS:
+            controller.is_d_pressed = True
+        elif action == glfw.RELEASE:
+            controller.is_d_pressed = False
+
+    # Caso de detecar la barra espaciadora, se cambia el metodo de dibujo --------------------------------------------Se debe activar el poder
+    if key == glfw.KEY_SPACE and action ==glfw.PRESS:
+        controller.fillPolygon = not controller.fillPolygon
+
+    elif key == glfw.KEY_ESCAPE and action ==glfw.PRESS:
+        glfw.set_window_should_close(window, True)
